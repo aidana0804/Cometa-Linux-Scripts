d9c63d21aab6b761f313f263069b023bcce522ce
aidana0804 <165639992+aidana0804@users.noreply.github.com>
2024-11-06 02:42:08 +0600

    Update script1_delete.sh

--- script1_delete.sh
+++ script1_delete.sh
@@ -1,50 +1,48 @@
 #!/bin/bash
 
-# 0. Удаление всех файлов из /tmp/result/
+# 0. Remove all files from /tmp/result/
 rm -rf /tmp/result/*
 mkdir -p /tmp/result
 
-# 1. Создание папок
+# 1. Create directories
 mkdir -p ~/resources/{disk,process,memory,user}
 
-# 2. Создание файлов
+# 2. Create files
 touch ~/resources/{d.txt,p.txt,m.txt,u.txt}
 
-# 3. Запись информации о свободном месте на дисках в d.txt
+# 3. Record information about available disk space in d.txt
 df -h > ~/resources/d.txt
 
-# 4. Запись информации о 10 самых ресурсоёмких процессах в p.txt
+# 4. Record information about the top 10 resource-intensive processes in p.txt
 ps -eo pid,user,%mem,%cpu,command --sort=-%mem | head -n 11 > ~/resources/p.txt
 
-# 5. Запись информации об оперативной памяти в m.txt
+# 5. Record information about memory usage in m.txt
 free -h > ~/resources/m.txt
 
-# 6. Запись информации о пользователе в u.txt
+# 6. Record user information in u.txt
 {
-    echo "Информация из /etc/passwd:"
+    echo "Information from /etc/passwd:"
     grep "^$(whoami):" /etc/passwd
-    echo -e "\nВремя последнего логина:"
+    echo -e "\nLast login time:"
     lastlog -u $(whoami)
-    echo -e "\nГруппы пользователя:"
+    echo -e "\nUser groups:"
     groups $(whoami)
-    echo -e "\nСодержимое домашней директории:"
+    echo -e "\nContents of the home directory:"
     ls -la ~
 } > ~/resources/u.txt
-# 7. Перемещение файлов и изменение их имён
+
+# 7. Move and rename files
 mv ~/resources/d.txt ~/resources/disk/disk.txt
 mv ~/resources/p.txt ~/resources/process/process.txt
 mv ~/resources/m.txt ~/resources/memory/memory.txt
 cp ~/resources/u.txt ~/resources/user/user.txt
 
-# 8. Смена группы и прав для папок и файлов
+# 8. Change group and permissions for directories and files
 sudo chgrp -R resgrp ~/resources/{disk,process,memory,user}
 sudo chmod -R 770 ~/resources/{disk,process,memory,user}
 
-# 9. Создание архива с текущей датой и временем
+# 9. Create an archive with the current date and time
 timestamp=$(date +"%Y%m%d_%H%M%S")
 tar -czf /tmp/result/resources_$timestamp.tar.gz -C ~/ resources
 
-echo "Скрипт выполнен успешно. Архив создан в /tmp/result/"
-
-
-
+echo "Script executed successfully. Archive created in /tmp/result/"
